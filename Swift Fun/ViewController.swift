ca466ec6b7d9cd55836211892c655f4445c2a0ad
Algy Mussá Abubacar Cassamo Ali Júnior <Algymussa@icloud.com>
2019-07-03 17:08:40 +0200

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by ALgy Aly on 7/3/19.
+//  Copyright © 2019 ALgy Aly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
