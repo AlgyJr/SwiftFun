acf79b07181a55479601f706b9160f4ed2cee770
Algy Jr <algymussa@gmail.com>
2019-07-09 18:14:00 +0200

    My second version

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var c = 0
+    @IBOutlet weak var lblFirst: UILabel!
+    
+    @IBAction func actionListener(_ sender: Any) {
+        c += 1
+        if c == 1 {
+            lblFirst.text = "Clicado \(c) vez"
+        } else {
+            lblFirst.text = "Clicado \(c) vezes"
+        }
+//        lblFirst.text = #"Chamo-me "Algy Mussá...""#
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
